1c7cc82e888c3826b9b9833c7a62145540ddd2a9
Nikita <baynik01@gmail.com>
2022-05-31 11:45:43 +0000

    4_2 dziala

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.util.Scanner;
 import java.io.IOException;
+import java.util.InputMismatchException;
 
 class WrongStudentName extends Exception { }
 class WrongAge extends Exception { }
@@ -27,8 +28,10 @@ class Main {
                 System.out.println("Błędny wiek!");
             } catch(WrongDateOfBirth e) {
                 System.out.println("Błędna data!");
+            } catch (InputMismatchException e) {
+             System.out.println("Podałeś liczbę w złym formacie");
             }
-        }
+    }
     }
 
     public static int menu() {
@@ -36,8 +39,14 @@ class Main {
         System.out.println("1 - aby dodać studenta");
         System.out.println("2 - aby wypisać wszystkich studentów");
         System.out.println("3 - aby wyszukać studenta po imieniu");
-        System.out.println("0 - aby wyjść z programu");
-        return scan.nextInt();
+        System.out.println("0 - aby wyjść z programu");             
+        var num = scan.nextInt();
+        if(num == '1'||num == '2'||num == '3'||num == '0'){
+          return num;
+        }
+        else{
+          throw new InputMismatchException();
+        }
     }
 
     public static String ReadName() throws WrongStudentName {
@@ -59,12 +68,12 @@ class Main {
         scan.nextLine();
         System.out.println("Podaj datę urodzenia DD-MM-YYY");
         String data = scan.nextLine();
-        if(data.charAt(2) == '-' && data.charAt(5) == '-'){
+          if(data.charAt(2) == '-' && data.charAt(5) == '-'){
             return data;
           }
-      else{
-        throw new WrongDateOfBirth();
-        }
+          else{
+            throw new WrongDateOfBirth();
+          }
     }
   
     public static void exercise1() throws IOException, WrongAge, WrongStudentName, WrongDateOfBirth {
